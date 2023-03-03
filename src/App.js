ae887a974d15d4f60c8bfec6fac7859b1934319d
Patryk <leniwysid@gmail.com>
2023-03-03 13:15:02 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import axios from "axios";
 import React, { useState } from "react";
 import foto from "./assets/foto.jpg";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
 
 function App() {
   const [city, setCity] = useState("");
   const [data, setData] = useState({});
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=807d004e98ade9ef529a71c7e287eea0
 `;
-  const location = (e) => {
-    if (e.key === "Enter") {
-      axios.get(url).then((res) => {
-        setData(res.data);
-        console.log(res.data);
-      });
-      setCity("");
-    }
+  const location = () => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+    setCity("");
   };
 
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      location();
+    }
+  };
   return (
     <div className="w-full h-screen relative">
       <img className="w-full h-screen object-cover" src={foto} alt="/" />
       <div className="absolute w-full h-full top-0 left-0 bg-gray-900/20"></div>
       <div className="absolute w-full text-white top-10 flex flex-col mx-auto text-3xl">
         <div className=" justify-between text-center ">
-          <div className="max-w-[1240px] h-[700px] mx-auto">
+          <div className="max-w-[1240px] h-[700px] mx-auto ">
             <input
               className="bg-gray-900/70 rounded-lg text-center p-2 mb-4"
               placeholder="Enter location"
-              onKeyDown={location}
               value={city}
               type="text"
               onChange={(e) => setCity(e.target.value)}
+              onKeyDown={handleKeypress}
             />
+            <button onClick={location} className="text-gray-400">
+              <HiArrowRightOnRectangle />
+            </button>
             <p className="mt-10 text-4xl">{data.name}</p>
             {data.main ? (
               <h1 className="text-8xl p-4">{data.main.temp.toFixed()} ℃</h1>
